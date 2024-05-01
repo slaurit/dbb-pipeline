4c2da02f0bce954623b2c8e23df4ef3ff38994af
Meena Desu <Meena.Desu@ibm.com>
2024-05-01 10:47:45 +0100

    moving for Demo

--- cics-genapp/base/src/COBOL/LGACDB01.cbl
+++ cics-genapp/base/src/COBOL/LGACDB01.cbl
@@ -25,7 +25,7 @@
       *  SAJ 06/13/2023
       *  SAJ DEVELOP
       *  CRP 07/10/2023
-      *  MEENA DEMO TESTM1
+      *  MEENA DEMO TESTM DEMO
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGACDB01.
